4625c667122684b9cc98fcf20836c1c4a701c677
ncvetinovic7619rn <ncvetinovic7619rn@raf.rs>
2022-03-20 18:18:47 +0100

    ispravljene greske v2

--- src/main/java/rs/raf/demo/services/PreduzeceService.java
+++ src/main/java/rs/raf/demo/services/PreduzeceService.java
@@ -1,20 +1,8 @@
 package rs.raf.demo.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import rs.raf.demo.model.Preduzece;
-import rs.raf.demo.repositories.PreduzeceRepository;
-
-import java.util.*;
 
 @Service
 public class PreduzeceService {
-
-    private final PreduzeceRepository preduzeceRepository;
-
-    @Autowired
-    public PreduzeceService(PreduzeceRepository preduzeceRepository) {
-        this.preduzeceRepository = preduzeceRepository;
-    }
 
 }
