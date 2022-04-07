fce73f2221a59672808b4379c0eb8b68dbcfd391
Mihajlo Stosic <mstosic2618rn@raf.rs>
2022-04-07 22:15:50 +0200

    Extract user service from monolith to microservice

---
+++ user-service/src/main/java/rs/raf/userservice/bootstrap/BootstrapData.java
@@ -0,0 +1,76 @@
+package rs.raf.userservice.bootstrap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import rs.raf.userservice.model.Permission;
+import rs.raf.userservice.model.User;
+import rs.raf.userservice.repositories.PermissionRepository;
+import rs.raf.userservice.repositories.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final Logger log = LoggerFactory.getLogger(BootstrapData.class);
+    private final UserRepository userRepository;
+    private final PermissionRepository permissionRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public BootstrapData(
+            UserRepository userRepository,
+            PermissionRepository permissionRepository,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.userRepository = userRepository;
+        this.permissionRepository = permissionRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) {
+
+        log.info("Loading Data...");
+
+        Permission permission1 = new Permission();
+        permission1.setName("permission1");
+        permissionRepository.save(permission1);
+
+        Permission permission2 = new Permission();
+        permission2.setName("permission2");
+        permissionRepository.save(permission2);
+
+        Permission permission3 = new Permission();
+        permission3.setName("permission3");
+        permissionRepository.save(permission3);
+
+        User user1 = new User();
+        user1.setUsername("user1");
+        user1.setPassword(this.passwordEncoder.encode("user1"));
+        user1.setFirstName("Petar");
+        user1.setLastName("Petrovic");
+
+        User user2 = new User();
+        user2.setUsername("user2");
+        user2.setPassword(this.passwordEncoder.encode("user2"));
+        user2.setFirstName("Marko");
+        user2.setLastName("Markovic");
+
+        List<Permission> user1Permissions = new ArrayList<>();
+        user1Permissions.add(permission1);
+        user1Permissions.add(permission2);
+        user1Permissions.add(permission3);
+        user1.setPermissions(user1Permissions);
+
+        this.userRepository.save(user1);
+        this.userRepository.save(user2);
+
+        log.info("Data loaded!");
+    }
+}
---
+++ user-service/src/main/java/rs/raf/userservice/controllers/PermissionRestController.java
@@ -0,0 +1,30 @@
+package rs.raf.userservice.controllers;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rs.raf.userservice.services.impl.PermissionService;
+
+@CrossOrigin
+@RestController
+@SecurityRequirement(name = "bearerAuth")
+@RequestMapping("/api/permissions")
+public class PermissionRestController {
+
+    private final PermissionService permissionService;
+
+    public PermissionRestController(PermissionService permissionService){
+        this.permissionService = permissionService;
+    }
+
+    @GetMapping(value = "/all",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getAllPermissions(){
+        return ResponseEntity.ok(permissionService.findAll());
+    }
+
+}
